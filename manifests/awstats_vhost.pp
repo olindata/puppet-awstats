cc9ccec4e471d2706d1f58233dbe65cb3e2efbed
Walter Heck <walter@tribily.com>
2012-05-07 08:21:45 +0800

    correct location of template

--- manifests/awstats_vhost.pp
+++ manifests/awstats_vhost.pp
@@ -1,23 +1,23 @@
 define awstats::awstats_vhost($ensure = present, $docroot, $outputdir, $user,
   $group, $domain, $aliases) {
-  file { "/etc/awstats/awstats.$name.conf":
-    ensure  => "$ensure",
+  file { "/etc/awstats/awstats.${name}.conf":
+    ensure  => $ensure,
     require => [
       Package[apache],
       File[$docroot],
       User[$name]
     ],
-    mode    => "644",
+    mode    => 0644,
     owner   => $user,
     group   => $group,
-    content => template("apache2/awstats.conf.erb"),
+    content => template('awstats/awstats.conf.erb'),
   }
 
 
 
-  cron { "awstats-update-run-$name":
+  cron { "awstats-update-run-${name}":
     ensure  => $ensure,
-    command => "/usr/bin/perl /usr/share/doc/awstats/examples/awstats_buildstaticpages.pl -config=$name -update -awstatsprog=/usr/lib/cgi-bin/awstats.pl -dir=${outputdir} -diricons=/awstats/awstats-icon/",
+    command => "/usr/bin/perl /usr/share/doc/awstats/examples/awstats_buildstaticpages.pl -config=${name} -update -awstatsprog=/usr/lib/cgi-bin/awstats.pl -dir=${outputdir} -diricons=/awstats/awstats-icon/",
     user    => $user,
     minute  => [1, 11, 21, 31, 41, 51],
     require => User[$user]
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,21 @@
 class awstats {
 
-  package { ["awstats"]:
-    ensure     => "present"
+  package { 'awstats':
+    ensure => present
   }
 
-    file { ["/etc/awstats"]:
-            ensure          => "directory",
-            mode            => 0755,
-            owner           => root,
-            group           => root,
-    }
+  file { '/etc/awstats':
+    ensure => directory,
+    mode   => 0755,
+    owner  => root,
+    group  => root,
+  }
 
-    file { ["/var/lib/awstats"]:
-            ensure          => "directory",
-            mode            => 0777,
-            owner           => www-data,
-            group           => www-data,
-    }
+  file { '/var/lib/awstats':
+    ensure => directory,
+    mode   => 0777,
+    owner  => www-data,
+    group  => www-data,
+  }
 
 }
